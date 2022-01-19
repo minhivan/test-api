7da9aeda3ffe9d9351fffde81aa5188ed26685ed
minh.pn <minh.pn811@gmail.com>
2022-01-19 14:46:21 +0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const exjwt = require('express-jwt');
+
+// Instantiating the express app
+const app = express();
+
+
+// See the react auth blog in which cors is required for access
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+    next();
+});
+
+// Setting up bodyParser to use json and set it to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const TOKEN = '78296248978335a6cbf24c4915a08a5a6905df73c3824d3b38c23f8d721a351a27c74b3418354bf3f02b527e53dbb22c63bf2a541196de4412f65a02d5fcbe9d'
+// INstantiating the express-jwt middleware
+const jwtMW = exjwt({
+    secret: TOKEN, algorithms: ['HS256']
+});
+
+
+// MOCKING DB just for test
+let users = [
+    {
+        id: 1,
+        username: 'test',
+        password: 'test123'
+    },
+    {
+        id: 2,
+        username: 'test2',
+        password: 'asdf12345'
+    }
+];
+
+// LOGIN ROUTE
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    // Use your DB ORM logic here to find user and compare password
+    for (let user of users) { // I am using a simple array users which i made above
+        if (username == user.username && password == user.password /* Use your password hash checking logic here !*/) {
+            //If all credentials are correct do this
+            let token = jwt.sign({ id: user.id, username: user.username }, '', { expiresIn: 129600 }); // Sigining the token
+            res.json({
+                sucess: true,
+                err: null,
+                token
+            });
+            break;
+        }
+        else {
+            res.status(401).json({
+                sucess: false,
+                token: null,
+                err: 'Username or password is incorrect'
+            });
+        }
+    }
+});
+
+app.get('/', jwtMW /* Using the express jwt MW here */, (req, res) => {
+    res.json({
+        sucess: true,
+        err: null,
+        message: "You are authenticated"
+    }); //Sending some response when authenticated
+});
+
+// Error handling 
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') { // Send the error rather than to show it on the console
+        res.status(401).send(err);
+    }
+    else {
+        next(err);
+    }
+});
+
+// Starting the app on PORT 3000
+const PORT = 8080;
+app.listen(PORT, () => {
+    // eslint-disable-next-line
+    console.log(`Magic happens on port ${PORT}`);
+});
